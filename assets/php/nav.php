a8005ac2a085f01234fee310c23068881df0a219
Aran77 <aransmithson@gmail.com>
2015-06-24 16:53:58 +0100

    more nav changes

--- assets/php/nav.php
+++ assets/php/nav.php
@@ -13,7 +13,6 @@ try {
 	$navlevel = "t";
 	$sth->bindparam(1, $navlevel, PDO::PARAM_STR);
 	$sth->execute();
-	$lastone = 0; 
 	while ($row = $sth->fetch()){
       
         if($row['tOrder'] == '1'){
@@ -23,24 +22,21 @@ try {
 			echo "<li><a href='".$row['Target']."'>".$row['Title']."</a></li>";
 		} elseif($row['tOrder'] > 2 && $row['tOrder'] < 99){
 			echo "<li><a href='".$row['Target']."'>".$row['Title']."</a></li>";
-            $lastone = 1;
 		}     ?>
-    <?php if($lastone==1){?></ul></div> 
+   <?php } ?>
+    </ul></div>
     <div class="pull-right">
     <ul class="nav navbar-nav" style="padding-top:5px">
-        <?php } ?>
-        <?php if(!isset($_SESSION['user'])){      ?>
-    	<li><button class="btn-sm btn-primary" data-toggle="modal"  id="login-btn" data-target="#login-form"><span class="glyphicon glyphicon-log-in sm">&nbsp;</span>Login</button></li> 
-        <?php  } else { 	?>
-       		<li><button class="btn-sm btn-primary" id="logout" onclick="locaton.href='assets/php/logout.php'"><span class="glyphicon glyphicon-log-out sm">&nbsp;</span>Logout</button></li>
-        <?php } ?>
-    </ul></div>
-	<?php } ?>
-    <?php
-    	if($_SESSION['isadmin']){ ?>
+    <?php if(!isset($_SESSION['user'])){      ?>
+    <li><button class="btn-sm btn-primary" data-toggle="modal"  id="login-btn" data-target="#login-form"><span class="glyphicon glyphicon-log-in sm">&nbsp;</span>Login</button></li> 
+    <?php  } else { 	?>
+       	<li><button class="btn-sm btn-primary" id="logout" onclick="locaton.href='assets/php/logout.php'"><span class="glyphicon glyphicon-log-out sm">&nbsp;</span>Logout</button></li>
+    <?php if($_SESSION['isadmin']){ ?>
    
         <li><button class="btn-sm btn-warning" onclick="location.href='/qms/admin/index.php'"><span class="glyphicon glyphicon-user sm">&nbsp;</span>Admin</button></li>
 		</ul></div>
+    <?php } ?>
 <?php } ?>
+</ul></div>	
 </div>
 </nav>
