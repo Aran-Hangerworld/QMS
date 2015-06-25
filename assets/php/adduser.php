3f9a057402ab9700c1392429351691d05a187e3c
benlee09 <ben.lee@hangerworld.co.uk>
2015-06-25 16:24:18 +0100

    stuff

--- assets/php/adduser.php
+++ assets/php/adduser.php
@@ -1,15 +1,16 @@
 <?php
-if (isset($_POST['QMS_user']) && isset($_POST['QMS_pass'])) {
+if (isset($_POST['QMS_lusername']) && isset($_POST['QMS_lpass'])) {
     include 'PDO.php';
-    $username= strip_tags($_POST['QMS_user']);
-    $realname =     strip_tags($_POST['QMS_Realnamae']);
-    $isadmin =   strip_tags($_POST['QMS_isadmin']);
+    $lusername= strip_tags($_POST['QMS_user']);
+    $rname =    strip_tags($_POST['QMS_realname']);
+    $email =    strip_tags($_POST['QMS_email']);
+    $isadmin =  strip_tags($_POST['QMS_isadmin']);
     $company = "Hangerworld";
     # Get new password
 	$response = CallAPI("GET","http://randomword.setgetgo.com/get.php");
 	$num = rand(10,99);
 	$newpasshash = md5(trim($response) . $num);
-	$newpass = trim($response) . $num;
+	$lpass = trim($response) . $num;
 	# End of password
 
 	try {
@@ -18,15 +19,15 @@ if (isset($_POST['QMS_user']) && isset($_POST['QMS_pass'])) {
 	    print "Error!: " . $e->getMessage();
     }
 	$sth = $db->prepare('CALL AdduserQMS(?,?,?,?)');
-	$sth->bindparam(1, $username, PDO::PARAM_STR);
-    $sth->bindparam(2, $realnamename,    PDO::PARAM_STR);
+	$sth->bindparam(1, $Lusername, PDO::PARAM_STR);
+    $sth->bindparam(2, $rname,    PDO::PARAM_STR);
 	$sth->bindparam(3, $newpasshash, PDO::PARAM_STR);
     $sth->bindparam(4, $isadmin,  PDO::PARAM_INT);
 	$sth->execute();
 	
 	# Email confirm
 	$sub = "New User Created - Hangerworld Suppport";
-	$msg = "Hi $realname,\r\n A new user has been created for you to use on the Hangerworld support site.\r\n Username: $username \r\n Password: $newpass \r\n Log in via http://www.hangerworld.co.uk/qms/ \r\n Regards \r\n IT Dept.";
+	$msg = "Hi $rname,\r\n A new user has been created for you to use on the Hangerworld support site.\r\n Username: $lusername \r\n Password: $lpass \r\n Log in via http://www.hangerworld.co.uk/qms/ \r\n Regards \r\n IT Dept.";
 	$headers = ""; 
 	mail($email,$sub,$msg,$headers);
 	 
@@ -39,4 +40,3 @@ if (isset($_POST['QMS_user']) && isset($_POST['QMS_pass'])) {
 	
 
 	
-INSERT INTO QMS_users (QMS_user, QMS_realname, QMS_email, pass, dept, isadmin) VALUES (username, rname, email, pass, dept, isadmin);
