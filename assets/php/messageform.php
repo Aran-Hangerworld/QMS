ee7ed9614ef5c35d60e345095fa3e241d41c44fa
benlee09 <ben.lee@hangerworld.co.uk>
2015-06-30 18:26:01 +0100

    message modal

---
+++ assets/php/messageform.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+        <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+        <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css"
+        rel="stylesheet" type="text/css">
+        <link href="http://pingendo.github.io/pingendo-bootstrap/themes/default/bootstrap.css"
+        rel="stylesheet" type="text/css">
+    </head>
+    
+    <body>
+        <div class="fade modal">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                        <h4 class="modal-title">Modal title</h4>
+                    </div>
+                    <div class="modal-body">
+                        <p>One fine body...</p>
+                        <input type="text" placeholder="Enter Message" id="messagebox">
+                    </div>
+                    <div class="modal-footer">
+                        <a class="btn btn-Warning btn-block" data-dismiss="modal">Delete</a>
+                        <a class="btn btn-block btn-primary">Submit</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+
+</html>
